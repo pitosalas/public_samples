422203cd11f37d7595757f31ca5abc4467ab28e9
Pito Salas <pitosalas@gmail.com>
2020-03-11 11:38:09 -0400

    latest

--- rails_examples/basic_auth_demo/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  resources :users
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  get 'welcome', to: 'sessions#welcome'
-  get 'logout', to: 'sessions#destroy'
-
-  root 'sessions#welcome'
-end
--- rails_examples/form_hidden_fields/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  resources :users
-
-  get :show_form, to: 'users#show_form'
-  get :do_form, to: 'users#do_form'
-  
-end
