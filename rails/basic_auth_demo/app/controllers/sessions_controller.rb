2aa56a11001db1f9ee225712aa9c6287de488178
Pito Salas <pitosalas@gmail.com>
2021-03-16 11:42:29 -0400

    new stuff

--- rails/basic_auth_demo/app/controllers/sessions_controller.rb
+++ rails/basic_auth_demo/app/controllers/sessions_controller.rb
@@ -3,9 +3,22 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(name: params[:name].downcase)
+    if user && user.authenticate(params[:password])
+      log_in user
+      redirect_to user
+    else
+      flash[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
   def login
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
   end
 
   def welcome
