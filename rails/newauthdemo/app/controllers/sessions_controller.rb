b652fbe733c251b41b293d4dd2eb2e2a8ff9e3d0
Pito Salas <pitosalas@gmail.com>
2022-10-28 07:47:01 -0400

    adding newauthdemo

---
+++ rails/newauthdemo/app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(name: params[:name])
+    if @user && @user.authenticate(params[:password])
+       session[:user_id] = @user.id
+       redirect_to '/welcome'
+    else
+       redirect_to '/login'
+    end
+  end
+
+  def login
+  end
+
+  def welcome
+  end
+end
---
+++ rails/newauthdemo/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  resources :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  #Add this to routes.rb
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
+  get 'logout', to: 'sessions#destroy'
+  root to: 'sessions#welcome'
+end
