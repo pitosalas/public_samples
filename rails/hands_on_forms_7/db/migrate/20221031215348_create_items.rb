4b2b368c33a38b62fafee58f16e0d3a722b74ac1
Pito Salas <pitosalas@gmail.com>
2022-10-31 21:11:47 -0400

    checkpoint

---
+++ rails/hands_on_forms_7/db/migrate/20221031215348_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[7.0]
+  def change
+    create_table :items do |t|
+      t.string :title
+      t.string :description
+      t.text :owner
+      t.integer :category_id
+
+      t.timestamps
+    end
+  end
+end
---
+++ rails/hands_on_forms_7/db/migrate/20221031215408_create_comments.rb
@@ -0,0 +1,11 @@
+class CreateComments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.string :from
+      t.string :message
+      t.integer :item_id
+
+      t.timestamps
+    end
+  end
+end
